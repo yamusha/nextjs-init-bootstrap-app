9cb669f96bfa67d4e5015f644de981090379e9f9
iFarm <mhukdham@gmail.com>
2021-05-16 15:31:35 +0700

    nextjs-init-bootstrap-app

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'bootstrap/dist/js/index.esm.js'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Sarabun:wght@400;500&display=swap"
+            rel="stylesheet"
+          >
+          </link>
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+            crossorigin="anonymous"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+            crossorigin="anonymous"
+          ></script>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
